f2c569bbffbe16335a02950a8e9cd2bdcfb4e523
Amandip Padda <amandippadda@outlook.com>
2023-11-05 19:53:43 -0600

    created component logoheader

---
+++ code/frontend/src/components/Register/Register.js
@@ -0,0 +1,17 @@
+import { Box, Card, Button, Input, Stack, Flex } from "@chakra-ui/react";
+import logo from "../assets/logo.svg";
+
+
+const Register = () => {
+  return (
+    <Box
+        fontFamily= "Lora"
+    >
+        <img src={logo} alt="logo" height="300" width="300" />
+        Connecting Shippers and Truckers Seamlessly
+    </Box>
+
+  );
+};
+
+export default Register;
