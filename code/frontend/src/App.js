ade14fd86047aeee26c0e458ff5b12fb985630ff
Ramanpreet Singh <rsa149@uregina.ca>
2023-11-05 23:05:14 -0600

    Add Shipper cards

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -2,13 +2,19 @@ import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
 import Register from './components/Register/register';
 import ResetPassword from './components/Login/Reset';
+import ShipperBusinessDetail from './components/shipper-business-details';
+import ShipperCompanyDetail from './components/shipper-company-details';
 
 function App() {
   return (
+    // <div className="App">
     // <Sidebar></Sidebar>
+    // </div>
     // <Login></Login>
       // <Register></Register>
-      <ResetPassword></ResetPassword>
+      // <ResetPassword></ResetPassword>
+      // <ShipperBusinessDetail></ShipperBusinessDetail>
+      <ShipperCompanyDetail></ShipperCompanyDetail>
   );
 }
 
