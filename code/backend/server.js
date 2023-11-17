acb5a803aad78b0fa23c4d9cbcee4f566aef67af
Ramanpreet Singh <rsa149@uregina.ca>
2023-11-17 13:45:24 -0600

    link company details

--- code/backend/server.js
+++ code/backend/server.js
@@ -1,73 +1,199 @@
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose'); 
-require('dotenv').config();
-const User = require ('./models/Users');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const User = require("./models/Users");
+const Shipper = require("./models/Shippers");
+const session = require('express-session');
+
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(session({
+  secret: process.env.KEY,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use((req, res, next) => {
+  console.log(req.session);
+  next();
+});
+
+
+
 // Connect to MongoDB
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
-connection.once('open',() => {
-    console.log('Database connected successfully.');
-})
+connection.once("open", () => {
+  console.log("Database connected successfully.");
+});
 
 /** Register Route*/
-app.post('/register', (req, res)=>{
-  const {email, password} = req.body;
-  User.findOne({email: email})
-  .then(user => {
-      if(user){
-          res.json("Already registered")
-      }
-      else{
-          User.create(req.body)
-          .then(user_form => res.json(user_form))
-          .catch(err => res.json(err))
-      }
-  })
-})
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      res.json("Already registered");
+    } else {
+      User.create(req.body)
+        .then((user_form) => res.json(user_form))
+        .catch((err) => res.json(err));
+    }
+  });
+});
 
-/** LogIn Route */
-app.post('/login', (req, res)=>{
-  const {email, password} = req.body;
-  User.findOne({email: email})
-  .then(user => {
-      if(user){
-          if(user.password === password) {
-              res.json("Success");
-          }
-          else{
-              res.json("Wrong password");
-          }
-      }
-      else{
-          res.json("No records found! ");
-      }
-  })
-})
+/** Shipper Business*/
+// app.post("/shipperBusinessDetail", (req, res) => {
+//   Shipper.create(req.body)
+//     .then((shipper_form) => res.json(shipper_form))
+//     .catch((err) => res.json(err));
+// });
+app.post("/shipperBusinessDetail", async (req, res) => {
+  try {
+    const businessNumber = req.body.businessNumber;
+    const proofOfBusiness = req.body.proofOfBusiness;
+    const proofOfInsurance = req.body.proofOfInsurance;
+    const name = req.body.name;
+    const phoneNumber = req.body.phoneNumber;
+    const email = req.body.email;
+    const password = req.body.password;
 
+    // Create Shipper instance
+    const shipper = new Shipper({
+      businessNumber,
+      proofOfBusiness,
+      proofOfInsurance,
+      name,
+      phoneNumber,
+      email,
+      password
+    });
 
-// Set up routes
-app.get('/', (req, res) => {
-  res.json('Hello, world!');
+    // Save Shipper to the database
+    const savedShipper = await shipper.save();
+
+    
+    req.session.email = email;
+    // res.redirect('/shipperCompanyDetail');
+    console.log('Email stored in session:', req.session.email);
+
+    res.json(savedShipper);
+    
+  } catch (err) {
+    res.json({ error: err.message });
+  }
+});
+
+/** Shipper company route ************/
+app.post("/shipperCompanyDetail", async (req, res) => {
+  try {
+    const email = req.session.email || 'No data found';
+    console.log(email);
+    const businessName = req.body.businessName;
+    const streetNumber = req.body.streetNumber;
+    const apartment = req.body.apartment;
+    const postalCode = req.body.postalCode;
+    const country = req.body.country;
+    const province = req.body.province;
+    const companyPhoneNumber = req.body.companyPhoneNumber;
+    const companyEmail = req.body.companyEmail;
+    const website = req.body.website;
+
+    // Find the Shipper by email
+    const shipper = await Shipper.findOne({ email });
+
+    if (!shipper) {
+      return res.status(404).json({ error: "Shipper not found with the provided email." });
+    }
+
+    // Update the Shipper's company details
+    shipper.businessName = businessName;
+    shipper.streetNumber = streetNumber;
+    shipper.apartment = apartment;
+    shipper.postalCode = postalCode;
+    shipper.country = country;
+    shipper.province = province;
+    shipper.companyPhoneNumber = companyPhoneNumber;
+    shipper.companyEmail = companyEmail;
+    shipper.website = website;
+
+    // Save the updated Shipper to the database
+    const savedShipper = await shipper.save();
+
+    res.json(savedShipper);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 
+/** Shipper company route ************/
+// app.post("/shipperCompanyDetail", async (req, res) => {
+//   try {
+//     const businessName = req.body.businessName;
+//     const streetNumber = req.body.streetNumber;
+//     const apartment = req.body.apartment;
+//     const postalCode = req.body.postalCode;
+//     const country = req.body.country;
+//     const province = req.body.province;
+//     const companyPhoneNumber = req.body.companyPhoneNumber;
+//     const companyEmail = req.body.companyEmail;
+//     const website = req.body.website;
 
+//     // Create Shipper instance
+//     const shipper = new Shipper({
+//       businessName,
+//       streetNumber,
+//     apartment,
+//     postalCode,
+//     country,
+//     province,
+//     companyPhoneNumber,
+//     companyEmail,
+//     website
+//     });
+
+//     // Save Shipper to the database
+//     const savedShipper = await shipper.save();
+
+//     res.json(savedShipper);
+//   } catch (err) {
+//     res.json({ error: err.message });
+//   }
+// });
+
+/** LogIn Route */
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("Success");
+      } else {
+        res.json("Wrong password");
+      }
+    } else {
+      res.json("No records found! ");
+    }
+  });
+});
+
+// Set up routes
+app.get("/", (req, res) => {
+  res.json("Hello, world!");
+});
 
 // Start the server
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-
 });
