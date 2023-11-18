e2dd76d922e3af720a0c060b23068cfebfc67207
Amandip Padda <amandippadda@outlook.com>
2023-11-18 17:46:04 -0600

    added shipper dashboard

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,6 +1,7 @@
 import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
 import Register from './components/Register/register';
+import ShipperSidebar from './components/Sidebar/ShipperSideBar.js';
 import ResetPassword from './components/ResetPassword/Reset';
 import ShipperBusinessDetail from './components/shipper-business-details';
 import ShipperCompanyDetail from './components/shipper-company-details';
@@ -12,7 +13,7 @@ function App() {
   return (
     <BrowserRouter >
         <Routes>
-        <Route path="/" element ={<Sidebar/>} />
+          <Route path="/" element ={<ShipperSidebar/>} />
           <Route path="/register" element ={<Signin/>} />
           <Route path="/login" element ={<Login/>} />
           <Route path="/resetPassword" element ={<ResetPassword/>} />
