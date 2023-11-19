ee307bd27e2cb6f594ed647e8d96d56dc145f23a
Ramanpreet Singh <rsa149@uregina.ca>
2023-11-19 14:11:00 -0600

    Post Load

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,9 +1,11 @@
 import Login from './components/Login/Login';
 import Sidebar from './components/Sidebar/Sidebar';
 import Register from './components/Register/register';
+import ShipperSidebar from './components/Sidebar/ShipperSideBar.js';
 import ResetPassword from './components/ResetPassword/Reset';
 import ShipperBusinessDetail from './components/shipper-business-details';
 import ShipperCompanyDetail from './components/shipper-company-details';
+import PostLoad from './components/Shipper/PostLoad.js'
 import Signin from './components/Signup/signup';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -12,7 +14,7 @@ function App() {
   return (
     <BrowserRouter >
         <Routes>
-        <Route path="/" element ={<Sidebar/>} />
+          <Route path="/" element ={<PostLoad/>} />
           <Route path="/register" element ={<Signin/>} />
           <Route path="/login" element ={<Login/>} />
           <Route path="/resetPassword" element ={<ResetPassword/>} />
