ec67dd8b81f751ffbf6d9cd01aaca1332098de17
Amandip Padda <amandippadda@outlook.com>
2023-11-19 13:10:59 -0600

    Chnaging SideBar

--- code/frontend/src/components/Shipper/PostLoad.js
+++ code/frontend/src/components/Shipper/PostLoad.js
@@ -1,5 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
-import ShipperSideBar from "../Sidebar/ShipperSideBar";
+import Sidebar from "../Sidebar/ShipperSideBar";
 import {
   Card,
   Input,
