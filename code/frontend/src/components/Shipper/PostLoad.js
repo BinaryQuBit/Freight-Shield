e194cce14c2ea06fb7845de7658b9e4ae51e4edc
Amandip Padda <amandippadda@outlook.com>
2023-11-19 12:58:11 -0600

    Link active page

--- code/frontend/src/components/Shipper/PostLoad.js
+++ code/frontend/src/components/Shipper/PostLoad.js
@@ -1,11 +1,11 @@
 import { Flex, Text } from '@chakra-ui/react'
 import Sidebar from '../Sidebar/ShipperSideBar'
 
-export default function ShipperSideBar() {
+export default function PostLoad() {
     return(
         <>
             <Flex>
-                <Sidebar/>
+                <Sidebar activePage="postLoad"/>
                 <Flex flex="1" alignItems="center" justifyContent="center">
                     <Text>Hello World</Text>
                 </Flex>
