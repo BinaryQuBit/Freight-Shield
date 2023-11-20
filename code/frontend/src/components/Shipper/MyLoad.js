e971ce6c0dbc8b14ea609e99b01c8972399d430d
Ramanpreet Singh <rsa149@uregina.ca>
2023-11-19 23:35:18 -0600

    Set Up my loads for shipper

--- code/frontend/src/components/Shipper/MyLoad.js
+++ code/frontend/src/components/Shipper/MyLoad.js
@@ -2,30 +2,69 @@ import { Flex, Text } from "@chakra-ui/react";
 import Sidebar from "../Sidebar/ShipperSideBar";
 import {
   Card,
-  Input,
-  Stack,
-  Center,
-  HStack,
-  Spacer,
-  VStack,
-  Checkbox,
-  SimpleGrid,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  Button,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Tbody,
 } from "@chakra-ui/react";
 import GreenButton from "../buttons/greenButton";
 import BlueButton from "../buttons/blueButton";
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export default function MyLoad() {
-    return(
+  const [loads, setLoads] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Fetch loads data from the server
+    axios.get("http://localhost:8080/myLoads")  // Update the endpoint to your actual API endpoint
+      .then((response) => {
+        setLoads(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching loads:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Flex>
         <Sidebar activePage="myLoads" />
-    )
+        <Flex flex={1} justifyContent="center">
+          <Card alignItems={"center"} maxWidth={1500} p={10}>
+            <Table variant="simple" spacing={6}>
+              <Thead>
+                <Tr>
+                  <Th>Pickup Address</Th>
+                  <Th>PickUp Date</Th>
+                  <Th>Pickup Time</Th>
+                  <Th>Drop off Address</Th>
+                  <Th>More Info</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {loads.map((load) => (
+                  <Tr key={load._id}>
+                    <Td>{load.pickUpLocation}</Td>
+                    <Td>{load.pickUpDate}</Td>
+                    <Td>{load.pickUpTime}</Td>
+                    <Td>{load.dropOffLocation}</Td>
+                    <Td>
+                      <GreenButton onClick={() => navigate(`/loadDetails/${load._id}`)}>
+                        More Info
+                      </GreenButton>
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Card>
+        </Flex>
+      </Flex>
+    </>
+  );
 }
