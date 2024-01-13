437745283cb31983c67a8de04d964cf5526413f5
paranjaa <paranjaa@uregina.ca>
2024-01-12 18:09:56 -0600

    Made a basic layout for the pending page

--- code/frontend/src/pages/admin/Pending.js
+++ code/frontend/src/pages/admin/Pending.js
@@ -1,5 +1,19 @@
 import Sidebar from "../../components/sidebar/AdminSideBar";
 import React, { useEffect } from "react";
+import { Button, ButtonGroup } from '@chakra-ui/react'
+import { IconButton } from "@chakra-ui/react";
+import {
+  FiMenu,
+  FiSettings,
+  FiTruck,
+  FiCompass,
+  FiBook,
+  FiLogOut,
+  FiHome,
+  FiCheckCircle,
+  FiXCircle
+} from "react-icons/fi";
+import { VStack, Card, Stack, HStack } from "@chakra-ui/react";
 import { Flex, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
@@ -27,11 +41,77 @@ export default function Pending() {
 
   return (
     <Flex>
-      <Sidebar activePage="Pending" />
-      <Flex flex="1" justifyContent="center">
-        <Text>Pending</Text>
-      </Flex>
-    </Flex>
+      <Sidebar activePage="pending" />
+      <Flex flex={1} justifyContent="center">
+        <VStack mt={10}>
 
+        <Text fontFamily="Lora" fontSize={25} fontWeight={"1000"} mt={2}>
+          Pending Users (basic layout)
+        </Text>
+
+
+
+            <Card
+              width="100%"
+              mb={200}
+              alignItems={"center"}
+              justifyContent={"center"}
+              p={10}
+            >
+                      <Text fontFamily="Lora" fontSize={20} fontWeight={"1000"} mt={2}>
+          Carriers
+        </Text>
+              <HStack spacing={4} p={4}>
+                    <Text fontFamily="Lora" fontWeight={"500"}>
+                      Company 1
+                    </Text>
+
+                    <IconButton colorScheme='green' aria-label='Approve' icon={<FiCheckCircle/>} />
+                    <IconButton colorScheme='red' aria-label='Deny'icon={<FiXCircle/>}  />
+              </HStack>
+
+    
+              <Text fontFamily="Lora" fontSize={20} fontWeight={"1000"} mt={2}>
+          Shippers
+          </Text>
+
+          <HStack spacing={4} p={4}>
+                    <Text fontFamily="Lora" fontWeight={"500"}>
+                      Shipper 1
+                    </Text>
+
+                    <IconButton colorScheme='green' aria-label='Approve' icon={<FiCheckCircle/>} />
+                    <IconButton colorScheme='red' aria-label='Deny'icon={<FiXCircle/>}  />
+              </HStack>
+
+          <Text fontFamily="Lora" fontSize={20} fontWeight={"1000"} mt={2}>
+          Drivers
+          </Text>
+
+          <HStack spacing={4} p={4}>
+                    <Text fontFamily="Lora" fontWeight={"500"}>
+                      Driver 1
+                    </Text>
+
+                    
+
+                    <IconButton colorScheme='green' aria-label='Approve' icon={<FiCheckCircle/>} />
+                    <IconButton colorScheme='red' aria-label='Deny'icon={<FiXCircle/>}  />
+              </HStack>
+
+          
+
+            </Card> 
+
+
+
+
+        </VStack>
+       
+      </Flex>
+
+    </Flex>
+    
+   
   );
 }
