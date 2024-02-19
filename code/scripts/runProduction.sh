c5875632992b39ea76292c367a778b0bdb567c63
Amandip Padda <amandippadda@outlook.com>
2024-02-19 16:06:00 -0600

    testing script

--- code/scripts/runProduction.sh
+++ code/scripts/runProduction.sh
@@ -1,14 +1,52 @@
 #!/bin/bash
 
-./runDevelopment
+FRONTEND_DIR="../frontend"
+BACKEND_DIR="../backend"
+SOURCE_DIR="$FRONTEND_DIR/build"
+DEST_DIR="$BACKEND_DIR/public/build"
 
-cd Freigh-Shield
+echo "Installing frontend dependencies..."
+cd "$FRONTEND_DIR"
+npm install
+if [ $? -eq 0 ]; then
+    echo "Dependencies installed successfully."
+else
+    echo "Error: Failed to install frontend dependencies."
+    exit 1
+fi
+
+echo "Building the frontend..."
+if npm run build; then
+    echo "Frontend built successfully."
+else
+    echo "Error: Failed to build frontend."
+    exit 1
+fi
+
+cd -
+
+if [ -d "$SOURCE_DIR" ]; then
+    echo "Copying build folder to backend..."
+    rm -rf "$DEST_DIR"
+    cp -R "$SOURCE_DIR" "$DEST_DIR"
+    echo "Build folder copied successfully."
+else
+    echo "Error: Build directory does not exist."
+    exit 1
+fi
+
+echo "Installing backend dependencies..."
+cd "$BACKEND_DIR"
+npm install
+if [ $? -eq 0 ]; then
+    echo "Backend dependencies installed successfully."
+else
+    echo "Error: Failed to install backend dependencies."
+    exit 1
+fi
 
 echo "Switching to the Production branch..."
 git checkout Production
-
-# echo "Pulling the latest changes from the Production branch..."
-# git pull origin Production
 
 echo "Adding changes from the backend directory..."
 cd code/backend
