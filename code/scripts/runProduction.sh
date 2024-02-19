b7c9a4abbb31ec3b259b8a1b6de606a30e1562ad
Amandip Padda <amandippadda@outlook.com>
2024-02-19 15:49:59 -0600

    Update backend with latest changes

---
+++ code/scripts/runProduction.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ./Freigh-Shield
+
+echo "Switching to the Production branch..."
+git checkout Production
+
+echo "Pulling the latest changes from the Production branch..."
+git pull origin Production
+
+echo "Adding changes from the backend directory..."
+cd code/backend
+git add .
+
+echo "Committing the changes..."
+git commit -m "Update backend with latest changes"
+
+echo "Pushing changes to the Production branch..."
+git push origin Production
+
+echo "Changes pushed to the Production branch successfully."
