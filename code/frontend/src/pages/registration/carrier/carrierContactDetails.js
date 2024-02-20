af9636702516bc84388a45d1c4336612336d8523
Amandip Padda <amandippadda@outlook.com>
2024-02-20 17:37:43 -0600

    removing cached

--- code/frontend/src/pages/registration/carrier/carrierContactDetails.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Protector from "../../../components/utils/methods/getters/protector.js";
-
-export default function CarrierContactDetails() {
-  Protector("/carriercontactdetails");
-  return (
-    <div>CarrierCompanyDetails</div>
-  )
-}
