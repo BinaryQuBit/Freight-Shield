df12e2c4ce135b3237f42d71db4fbf53e6146f6f
Amandip Padda <amandippadda@outlook.com>
2024-02-20 16:28:49 -0600

    bringig back the files

---
+++ code/backend/controllers/adminController.js
@@ -0,0 +1,75 @@
+import asyncHandler from "express-async-handler";
+
+// @desc    Getting Administrators
+// route    GET /administartors
+// @access  Private
+const administrators = asyncHandler(async (req, res) => {
+  const user = {
+    _id: req.user._id,
+    email: req.user.email,
+  };
+
+  res.status(200).json({ user });
+});
+
+// @desc    Getting Pending
+// route    GET /pending
+// @access  Private
+const pending = asyncHandler(async (req, res) => {
+    const user = {
+      _id: req.user._id,
+      email: req.user.email,
+    };
+  
+    res.status(200).json({ user });
+  });
+
+// @desc    Getting Approved
+// route    GET /approved
+// @access  Private
+const approved = asyncHandler(async (req, res) => {
+    const user = {
+      _id: req.user._id,
+      email: req.user.email,
+    };
+  
+    res.status(200).json({ user });
+  });
+
+// @desc    Getting Shippers
+// route    GET /shippers
+// @access  Private
+const shippers = asyncHandler(async (req, res) => {
+    const user = {
+      _id: req.user._id,
+      email: req.user.email,
+    };
+  
+    res.status(200).json({ user });
+  });
+
+// @desc    Getting Carriers
+// route    GET /carriers
+// @access  Private
+const carriers = asyncHandler(async (req, res) => {
+    const user = {
+      _id: req.user._id,
+      email: req.user.email,
+    };
+  
+    res.status(200).json({ user });
+  });
+
+// @desc    Getting Carriers
+// route    GET /carriers
+// @access  Private
+const adminsettings = asyncHandler(async (req, res) => {
+    const user = {
+      _id: req.user._id,
+      email: req.user.email,
+    };
+  
+    res.status(200).json({ user });
+  });
+
+export { administrators, pending, approved, shippers, carriers, adminsettings }
---
+++ code/backend/models/marketplaceModel.js
@@ -0,0 +1,85 @@
+// Model for Marketplace which details of loads posted
+import mongoose from "mongoose";
+
+const marketplaceSchema = mongoose.Schema(
+  {
+    email: {
+      type: String,
+    },
+    loadNumber: {
+      type: String,
+    },
+    pickUpLocation: {
+      type: String,
+      required: true,
+    },
+    pickUpDate: {
+      type: String,
+      required: true,
+    },
+    pickUpTime: {
+      type: String,
+      required: true,
+    },
+    dropOffDate: {
+      type: String,
+      required: true,
+    },
+    dropOffTime: {
+      type: String,
+      required: true,
+    },
+    dropOffLocation: {
+      type: String,
+      required: true,
+    },
+    unitRequested: {
+      type: String,
+      required: true,
+    },
+    typeLoad: {
+      type: String,
+      required: true,
+    },
+    sizeLoad: {
+      type: String,
+    },
+    additionalInformation: {
+      type: String,
+    },
+    additionalDocument: {
+      type: String,
+    },
+    pickUpCity: {
+      type: String,
+    },
+    dropOffCity:{
+      type: String,
+    },
+    pickUpLAT: {
+      type: String,
+    },
+    pickUpLNG: {
+      type: String,
+    },
+    dropOffLAT:{
+      type: String,
+    },
+    dropOffLNG: {
+      type: String,
+    },
+    status:{
+      type: String,
+    },
+    carrierEmail:{
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Marketplace = mongoose.model("Marketplace", marketplaceSchema);
+
+export default Marketplace;
