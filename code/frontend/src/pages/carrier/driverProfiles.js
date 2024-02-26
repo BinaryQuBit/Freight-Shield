538785ba42560677d86dc99816b75f98e55c8976
Amandip Padda <amandippadda@outlook.com>
2024-02-26 00:53:01 -0600

    updating

--- code/frontend/src/pages/carrier/driverProfiles.js
+++ code/frontend/src/pages/carrier/driverProfiles.js
@@ -34,19 +34,24 @@ import { useData } from "../../components/utils/methods/getters/dataContext.js";
 import CustomButton from "../../components/buttons/customButton.js";
 
 // Start of Build
+// Start of Build
 export default function DriverProfiles() {
   const { data } = useData();
   Protector("/api/driverprofiles");
   const driverData = data.driverData || [];
 
+  const pendingDrivers = driverData.filter(driver => driver.driverStatus === 'pending');
+  const acceptedDrivers = driverData.filter(driver => driver.driverStatus === 'accepted');
+
   // Hooks
   const { colorMode } = useColorMode();
-
   // Functions
   const handleDecline = async (driverId, event) => {
     event.preventDefault();
     try {
-      const driverStatusResponse = await axios.put(`/api/updatedriverstatus/${driverId}`, {}, {
+      const driverStatusResponse = await axios.put(`/api/updatedriverstatus/${driverId}`, {
+        status: 'decline'
+      }, {
         withCredentials: true,
       });
 
@@ -62,93 +67,141 @@ export default function DriverProfiles() {
     }
   };
 
-
-  const handleAccept = async (event) => {
+  const handleAccept = async (driverId, event) => {
     event.preventDefault();
-  }
+    try {
+      const driverStatusResponse = await axios.put(`/api/updatedriverstatus/${driverId}`, {
+        status: 'accepted'
+      }, {
+        withCredentials: true,
+      });
 
+      if (driverStatusResponse.status === 200) {
+        window.location.reload();
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        console.error("Error: ", error.response.data.message);
+      } else {
+        console.error("Error submitting form:", error);
+      }
+    }
+  };
+
+    // Check for no drivers
+    const noDrivers = driverData.length === 0;
+
+  // Component rendering
   return (
     <>
       <Sidebar activePage={"driverProfile"} />
       <EaseOut>
         <UserHeader title={"Driver Profiles"} />
         <Flex pt="10" direction="column" padding="10">
-          <Card overflowX="auto" width="full" p="4">
-            <Accordion allowToggle>
-              {driverData.filter(driver => !driver.driverStatus || driver.driverStatus === 'pending').map((driver) => (
-                <AccordionItem key={driver.email}>
-                  <h2>
-                    <AccordionButton
-                      _expanded={{
-                        bg: colorMode === "dark" ? "blue.700" : "blue.100",
-                        color: colorMode === "dark" ? "white" : "black",
-                      }}
-                    >
-                      <Box flex="1">
-                        <Text fontSize="lg">
-                          {driver.firstName.trim()} {driver.lastName}
-                        </Text>
-                      </Box>
-                      <AccordionIcon />
-                    </AccordionButton>
-                  </h2>
-                  <AccordionPanel pb={4}>
-                    <Stack>
-                      <Flex justifyContent="space-between">
-                        <Box>
-                          <Text fontSize="md" mb="2">
-                            <strong>Email:</strong> {driver.email}
-                          </Text>
-                          <Text fontSize="md" mb="2">
-                            <strong>Phone Number:</strong> {driver.phoneNumber}
-                          </Text>
-                        </Box>
-                        <VStack>
-                          <Link
-                            href={`http://localhost:8080/${driver.driverAbstract}`}
-                            isExternal
-                            color="blue.500"
-                          >
-                            View Driver Abstract
-                          </Link>
-                          <Link
-                            href={`http://localhost:8080/${driver.driverLicence}`}
-                            isExternal
-                            color="blue.500"
-                            mt={2}
-                          >
-                            View Driver Licence
-                          </Link>
-                        </VStack>
-                      </Flex>
-                    </Stack>
-                    <Flex justifyContent={"space-between"} mt={"7"}>
-                      <CustomButton
-                        backgroundColor="#D22B2B"
-                        icon={<IoMdCloseCircle />}
-                        mt="4"
-                        w="100px"
-                        children="Decline"
-                        variant="blueBackwardButton"
-                        onClick={(e) => handleDecline(driver.driver_id, e)}
-                      />
-                      <CustomButton
-                        backgroundColor="#42B72A"
-                        icon={<IoMdAddCircle />}
-                        mt="4"
-                        w="100px"
-                        onClick={(e) => handleAccept(e)}
-                        children="Accept"
-                        variant="blueForwardButton"
-                      />
-                    </Flex>
-                  </AccordionPanel>
-                </AccordionItem>
-              ))}
-            </Accordion>
-          </Card>
+          {noDrivers ? (
+            <Text fontSize="20" textAlign={"center"}>Chirp Chirp, No Drivers Here</Text>
+          ) : (
+            <Card overflowX="auto" width="full" p="4">
+              {pendingDrivers.length > 0 && (
+                <>
+                  <Text fontSize="20" mb="4" textAlign={"center"}><strong>Pending</strong></Text>
+                  <Accordion allowToggle>
+                    {pendingDrivers.map((driver) => (
+                      <DriverProfileItem driver={driver} colorMode={colorMode} handleDecline={handleDecline} handleAccept={handleAccept} />
+                    ))}
+                  </Accordion>
+                </>
+              )}
+              {acceptedDrivers.length > 0 && (
+                <>
+                  <Text fontSize="20" textAlign={"center"} mb="4" mt="5"><strong>Accepted</strong></Text>
+                  <Accordion allowToggle>
+                    {acceptedDrivers.map((driver) => (
+                      <DriverProfileItem driver={driver} colorMode={colorMode} />
+                    ))}
+                  </Accordion>
+                </>
+              )}
+            </Card>
+          )}
         </Flex>
       </EaseOut>
     </>
   );
 }
+
+function DriverProfileItem({ driver, colorMode, handleDecline, handleAccept }) {
+  return (
+    <AccordionItem key={driver.email}>
+      <h2>
+        <AccordionButton
+          _expanded={{
+            bg: colorMode === "dark" ? "blue.700" : "blue.100",
+            color: colorMode === "dark" ? "white" : "black",
+          }}
+        >
+          <Box flex="1">
+            <Text fontSize="lg">
+              {driver.firstName.trim()} {driver.lastName}
+            </Text>
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>
+        <Stack>
+          <Flex justifyContent="space-between">
+            <Box>
+              <Text fontSize="md" mb="2">
+                <strong>Email:</strong> {driver.email}
+              </Text>
+              <Text fontSize="md" mb="2">
+                <strong>Phone Number:</strong> {driver.phoneNumber}
+              </Text>
+            </Box>
+            <VStack>
+              <Link
+                href={`http://localhost:8080/${driver.driverAbstract}`}
+                isExternal
+                color="blue.500"
+              >
+                View Driver Abstract
+              </Link>
+              <Link
+                href={`http://localhost:8080/${driver.driverLicence}`}
+                isExternal
+                color="blue.500"
+                mt={2}
+              >
+                View Driver Licence
+              </Link>
+            </VStack>
+          </Flex>
+        </Stack>
+        {driver.driverStatus === 'pending' && (
+          <Flex justifyContent={"space-between"} mt={"7"}>
+            <CustomButton
+              backgroundColor="#D22B2B"
+              icon={<IoMdCloseCircle />}
+              mt="4"
+              w="100px"
+              children="Decline"
+              variant="blueBackwardButton"
+              onClick={(e) => handleDecline(driver.driver_id, e)}
+            />
+            <CustomButton
+              backgroundColor="#42B72A"
+              icon={<IoMdAddCircle />}
+              mt="4"
+              w="100px"
+              onClick={(e) => handleAccept(driver.driver_id, e)}
+              children="Accept"
+              variant="blueForwardButton"
+            />
+          </Flex>
+        )}
+      </AccordionPanel>
+    </AccordionItem>
+  );
+}
+
