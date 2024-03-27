20ed6b58ae437ef57c4570f65b28b10ed8056efa
M-Alharbi <alharmoh@uregina.ca>
2024-03-27 00:35:41 -0600

    adding dark mode to all screens

--- code/mobile/screens/SettingScreen.js
+++ code/mobile/screens/SettingScreen.js
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
   Linking,
   Alert,
+  TextInput,
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import axios from "axios";
@@ -16,6 +17,7 @@ import {
   faHeadset,
   faTruck,
   faEdit,
+  faSave,
 } from "@fortawesome/free-solid-svg-icons";
 import { useNavigation } from "@react-navigation/native";
 import { useTheme } from "../components/themeContext.js";
@@ -28,7 +30,22 @@ export default function SettingScreen(props) {
   const isFocused = useIsFocused();
 
   // Hooks
-  const [driverData, setDriverData] = useState("");
+  const [driverData, setDriverData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    canadianCarrierCode: "",
+    driverLicenceFront: "",
+    driverLicenceBack: "",
+    driverAbstract: "",
+  });
+  const [editableFields, setEditableFields] = useState({
+    firstName: false,
+    lastName: false,
+    email: false,
+    phoneNumber: false,
+  });
 
   const logout = () => {
     axios
@@ -39,6 +56,13 @@ export default function SettingScreen(props) {
       .catch((error) => {
         console.error("Logout failed:", error);
       });
+  };
+
+  const toggleEdit = (field) => {
+    setEditableFields((currentFields) => ({
+      ...currentFields,
+      [field]: !currentFields[field],
+    }));
   };
 
   useEffect(() => {
@@ -84,6 +108,13 @@ export default function SettingScreen(props) {
     Alert.alert("Edit", `Edit`);
   };
 
+  const handleChange = (field, value) => {
+    setDriverData((currentData) => ({
+      ...currentData,
+      [field]: value,
+    }));
+  };
+
   return (
     <View
       style={[
@@ -92,79 +123,115 @@ export default function SettingScreen(props) {
       ]}
     >
       <ScrollView style={styles.container}>
-        <View style={styles.settingItem}>
+        <View
+          style={[
+            styles.toggleContainer,
+            {
+              backgroundColor: isDarkMode ? "#222" : "#EEE",
+              marginHorizontal: 20,
+            },
+          ]}
+        >
           <Text
             style={[
-              styles.settingText,
+              styles.toggleLabel,
               { color: isDarkMode ? "#FFF" : "#333" },
             ]}
           >
-            Dark Mode
+            {isDarkMode ? "Dark Mode" : "Light Mode"}
           </Text>
-          <Switch value={isDarkMode} onValueChange={toggleTheme} />
-        </View>
-<View>
-  <Text>Driver Information</Text>
-</View>
-        <View>
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            First Name: {driverData.firstName}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Last Name: {driverData.lastName}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Email: {driverData.email}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Canadian Carrier Code: {driverData.canadianCarrierCode}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Phone Number: {driverData.phoneNumber}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Driver Licence (Front): {driverData.driverLicenceFront}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Driver Licence (Back): {driverData.driverLicenceBack}
-          </Text>
-
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}>
-            Driver Abstract: {driverData.driverAbstract}
-          </Text>
-          <Text style={{ color: isDarkMode ? "#FFF" : "#333" }}></Text>
-        </View>
-
-        <TouchableOpacity onPress={() => handleEdit()}>
-          <FontAwesomeIcon
-            icon={faEdit}
-            size={20}
-            style={{ color: isDarkMode ? "#FFF" : "#333" }}
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isDarkMode ? "#f5dd4b" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={toggleTheme}
+            value={isDarkMode}
           />
-        </TouchableOpacity>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text
+            style={[styles.infoTitle, { color: isDarkMode ? "#FFF" : "#333" }]}
+          >
+            Driver Information
+          </Text>
+          {Object.keys(driverData).map((key) => (
+            <View
+              key={key}
+              style={[
+                styles.infoRow,
+                { borderColor: isDarkMode ? "#555" : "#ccc" },
+              ]}
+            >
+              {editableFields[key] ? (
+                <>
+                  <Text
+                    style={{
+                      color: isDarkMode ? "#FFF" : "#333",
+                      marginRight: 5,
+                    }}
+                  >
+                    {`${key.charAt(0).toUpperCase() + key.slice(1)}:`}
+                  </Text>
+                  <TextInput
+                    style={[
+                      styles.input,
+                      {
+                        backgroundColor: isDarkMode ? "#555" : "#FFF",
+                        color: isDarkMode ? "#FFF" : "#333",
+                      },
+                    ]}
+                    onChangeText={(text) => handleChange(key, text)}
+                    value={driverData[key]}
+                  />
+                </>
+              ) : (
+                <Text
+                  style={[
+                    styles.infoText,
+                    { color: isDarkMode ? "#FFF" : "#333", flex: 1 },
+                  ]}
+                >
+                  {`${key.charAt(0).toUpperCase() + key.slice(1)}: ${
+                    driverData[key]
+                  }`}
+                </Text>
+              )}
+              <TouchableOpacity
+                onPress={() => toggleEdit(key)}
+                style={styles.editButton}
+              >
+                <FontAwesomeIcon
+                  icon={editableFields[key] ? faSave : faEdit}
+                  size={20}
+                  style={{ color: isDarkMode ? "#FFF" : "#0866FF" }}
+                />
+              </TouchableOpacity>
+            </View>
+          ))}
+        </View>
+        <View style={styles.bottomButtons}>
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#0866FF" }]}
+            onPress={logout}
+          >
+            <FontAwesomeIcon
+              icon={faSignOutAlt}
+              size={20}
+              style={styles.icon}
+            />
+            <Text style={styles.buttonText}>Logout</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.button, { backgroundColor: "#0866FF" }]}
+            onPress={() => handleSupport("Customer")}
+          >
+            <FontAwesomeIcon icon={faHeadset} size={20} style={styles.icon} />
+            <Text style={styles.buttonText}>Customer Support</Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
-
-      <View style={styles.bottomButtons}>
-        <TouchableOpacity style={styles.button} onPress={logout}>
-          <FontAwesomeIcon icon={faSignOutAlt} style={styles.icon} />
-          <Text style={styles.buttonText}>Logout</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => handleSupport("Customer")}
-        >
-          <FontAwesomeIcon icon={faHeadset} style={styles.icon} />
-          <Text style={styles.buttonText}>Customer Support</Text>
-        </TouchableOpacity>
-
-      </View>
     </View>
   );
 }
@@ -179,19 +246,6 @@ const styles = StyleSheet.create({
     flex: 1,
 
     padding: 20,
-  },
-  settingItem: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingVertical: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#e1e1e1",
-    marginBottom: 20,
-  },
-  settingText: {
-    fontSize: 16,
-    color: "#333",
   },
   icon: {
     color: "#FFFFFF",
@@ -212,9 +266,8 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   bottomButtons: {
-    padding: 20,
     paddingBottom: 30,
-    marginBottom: 40,
+    marginBottom: 60,
   },
   infoContainer: {
     padding: 10,
@@ -225,7 +278,7 @@ const styles = StyleSheet.create({
   infoTitle: {
     fontSize: 20,
     fontWeight: "bold",
-    marginBottom: 10,
+    marginBottom: 20,
   },
   infoText: {
     fontSize: 16,
@@ -233,5 +286,39 @@ const styles = StyleSheet.create({
   },
   titleHeader: {
     fontSize: "20px",
+  },
+  infoRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderColor: "#ccc",
+    paddingBottom: 10,
+  },
+  editButton: {
+    marginLeft: 10,
+  },
+  input: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+  toggleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 20,
+    borderRadius: 10,
+    marginHorizontal: 20,
+  },
+  toggleLabel: {
+    fontSize: 16,
+    fontWeight: "500",
   },
 });
