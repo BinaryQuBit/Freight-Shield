b72b9fbb038fee718c3df087b7b1187257bad46b
Ramanpreet Singh <rsa149@uregina.ca>
2024-03-29 14:53:52 -0600

    Add tests for Login Page

--- code/frontend/src/components/forms/tests/loginForm.test.js
+++ code/frontend/src/components/forms/tests/loginForm.test.js
@@ -1,9 +1,21 @@
 import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter
+import { MemoryRouter } from "react-router-dom";
 import LoginForm from "../loginForm";
 
+const renderForm = () => {
+  return render(
+    <MemoryRouter>
+      <LoginForm />
+    </MemoryRouter>
+  );
+};
+
 describe("Log in form ", () => {
-    it("should always pass", () => {
-        expect(true).toBe(true);
-    });
-})
+  it("renders without crashing", () => {
+    const { getByText } = renderForm();
+    expect(getByText("Log In")).toBeInTheDocument();
+    expect(getByText("Forgot Password")).toBeInTheDocument();
+    expect(getByText("Create an Account")).toBeInTheDocument();
+    expect(getByText("Your Ultimate Loadboard Solution!")).toBeInTheDocument();
+  });
+});
