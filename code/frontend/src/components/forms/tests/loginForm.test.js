d887a2d599ce2fc22459b91ff1febf813ba2b2d5
Ramanpreet Singh <rsa149@uregina.ca>
2024-03-29 14:45:56 -0600

    Set up jest and RTL environment

---
+++ code/frontend/src/components/forms/tests/loginForm.test.js
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom"; // Import MemoryRouter
+import LoginForm from "../loginForm";
+
+describe("Log in form ", () => {
+    it("should always pass", () => {
+        expect(true).toBe(true);
+    });
+})
