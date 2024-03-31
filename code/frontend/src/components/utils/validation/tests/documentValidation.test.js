e542384164eab3f6890f40b9bedd96a90eddef25
Ramanpreet Singh <rsa149@uregina.ca>
2024-03-31 14:03:52 -0600

    Test for document and email validation

---
+++ code/frontend/src/components/utils/validation/tests/documentValidation.test.js
@@ -0,0 +1,17 @@
+import { DocumentValidation } from '../documentValidation'
+
+describe('DocumentValidation', () => {
+  test('returns error message when value is empty', () => {
+    const name = 'Field';
+    const value = '';
+    const errorMessage = DocumentValidation(name, value);
+    expect(errorMessage).toBe(`${name} is required`);
+  });
+
+  test('returns empty string when value is not empty', () => {
+    const name = 'Field';
+    const value = '12345';
+    const errorMessage = DocumentValidation(name, value);
+    expect(errorMessage).toBe('');
+  });
+});
